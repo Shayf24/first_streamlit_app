394d1784c06e928a3cd54aad8f766e6aada2f7ad
Shayf24 <79882940+Shayf24@users.noreply.github.com>
2023-06-12 18:49:26 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,11 @@
 import streamlit
 
-import pandas
 
-import requests
 
-import snowflake 
 
-import snowflake.connector
 
-from urllib.error import URLError
+
+
 
 streamlit.title('My Parents new healthy dinner')
 streamlit.header('Breakfast Menu')
@@ -16,7 +13,7 @@ streamlit.text('🥣 🥗  🥑🍞 Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 
-
+import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
 
@@ -27,7 +24,7 @@ streamlit.dataframe(my_fruit_list)
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), key = 'Lime')
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show )
-
+import requests
 #new request
 
 
@@ -44,3 +41,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 
+import snowflake 
+
+import snowflake.connector
+from urllib.error import URLError
